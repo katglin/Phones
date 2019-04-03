1c1acc164ca9c4e9d33ae3722c543aed48fbde7d
katglin <katerinaglinskaya96@gmail.com>
2019-04-03 18:14:13 +0300

    added toogle

--- Phones/App_Start/BundleConfig.cs
+++ Phones/App_Start/BundleConfig.cs
@@ -24,7 +24,8 @@ namespace Phones
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/Content/site.css",
+                      "~/Content/toogle.css"));
         }
     }
 }
