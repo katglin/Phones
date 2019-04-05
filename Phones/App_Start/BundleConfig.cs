c04f2826c3dfe824c810ba79a1f890a411dab1f2
katglin <katerinaglinskaya96@gmail.com>
2019-04-05 11:41:01 +0300

    added themes and sass

--- Phones/App_Start/BundleConfig.cs
+++ Phones/App_Start/BundleConfig.cs
@@ -24,8 +24,7 @@ namespace Phones
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                      "~/Content/toogle.css"));
+                      "~/Content/main.css"));
         }
     }
 }
